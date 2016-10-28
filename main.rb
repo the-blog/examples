fe04fc07f977a3a44621337b437a9ad2e5bf9271
Ilya N. Zykin <ilya.zykin@toptal.com>
2016-10-28 10:42:06 +0300

    example 7

--- main.rb
+++ main.rb
@@ -3,7 +3,19 @@ require_relative 'exit'
 puts "Type the command"
 exit_instruction
 
+def commands_processor(cmd)
+  if cmd == 'command 1'
+    puts 'Command 1'
+  elsif cmd == 'command 2'
+    puts 'Command 2'
+  else
+    puts 'Unknown command'
+  end
+end
+
 loop do
-  cmd = STDIN.gets.chomp
+  cmd = STDIN.gets.chomp.downcase
   exit_command?(cmd) && break
+
+  commands_processor(cmd)
 end
