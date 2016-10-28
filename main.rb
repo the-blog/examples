d7a7dcf431620d5d1641d9eb1b0a27f86b6172ca
Ilya N. Zykin <ilya.zykin@toptal.com>
2016-10-28 10:43:02 +0300

    example 8

--- main.rb
+++ main.rb
@@ -4,12 +4,13 @@ puts "Type the command"
 exit_instruction
 
 def commands_processor(cmd)
-  if cmd == 'command 1'
-    puts 'Command 1'
-  elsif cmd == 'command 2'
-    puts 'Command 2'
-  else
-    puts 'Unknown command'
+  case cmd
+    when 'command 1'
+      puts 'Command 1'
+    when 'command 2'
+      puts 'Command 2'
+    else
+      puts 'Unknown command'
   end
 end
 
