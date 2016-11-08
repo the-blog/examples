806e13fda39389e9a685555abcf63dd341e2c015
Ilya N. Zykin <ilya.zykin@toptal.com>
2016-11-08 22:03:15 +0300

    example 12

--- main_command_processor.rb
+++ main_command_processor.rb
@@ -1,6 +1,7 @@
 module MainCommandProcessor
-  def self.manual
-    """
+  class << self
+    def manual
+      """
 Main Command Processor allow to use the following commands:
 
 1. `command 1` will show you text `Command 1`
@@ -8,17 +9,18 @@ Main Command Processor allow to use the following commands:
 3. Any other command will show you `Unknown command`
 
 Please type a command.
-    """
-  end
+      """
+    end
 
-  def self.process(cmd)
-    case cmd
-      when 'command 1'
-        puts 'Command 1'
-      when 'command 2'
-        puts 'Command 2'
-      else
-        puts 'Unknown command'
+    def process(cmd)
+      case cmd
+        when 'command 1'
+          puts 'Command 1'
+        when 'command 2'
+          puts 'Command 2'
+        else
+          puts 'Unknown command'
+      end
     end
   end
 end
