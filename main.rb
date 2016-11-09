8f0f49bfc0e0c59939cae3291e4496067c8c3965
Ilya N. Zykin <ilya.zykin@toptal.com>
2016-11-09 23:49:54 +0300

    example 19

--- main.rb
+++ main.rb
@@ -2,9 +2,9 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default)
 
-require_relative 'exit_processor'
-require_relative 'main_command_processor'
-require_relative 'command_line_helper'
+require_relative 'processors/exit_processor'
+require_relative 'processors/main_command_processor'
+require_relative 'helpers/command_line_helper'
 
 puts ExitProcessor.manual
 MainCommandProcessor.cli
