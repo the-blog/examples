5a4fe3ae0c3d2b5ec2b32a1ee228015ab4f0cd74
Ilya N. Zykin <zykin-ilya@ya.ru>
2016-11-17 23:15:49 +0300

    example 27

--- models/bill.rb
+++ models/bill.rb
@@ -1,8 +1,25 @@
 class Bill
   attr_accessor :title, :cost
+  attr_reader :errors
 
   def initialize
     @title = 'No Title'
     @cost  = 0.0
+
+    @errors = []
+  end
+
+  def valid?
+    @errors = []
+
+    if !@title.is_a?(String)
+      @errors << "Title is not a String"
+    end
+
+    if !@cost.is_a?(Numeric)
+      @errors << "Cost is not a Numeric value"
+    end
+
+    @errors.empty?
   end
 end
