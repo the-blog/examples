8ef1508e00ae0609a59ee9b5c7f4b469602f257c
Ilya N. Zykin <zykin-ilya@ya.ru>
2016-11-17 23:35:44 +0300

    example 29

--- models/bill.rb
+++ models/bill.rb
@@ -21,14 +21,40 @@ class Bill
   private
 
   def validate_title
-    if !@title.is_a?(String)
+    unless @title.is_a?(String)
       @errors << "Title is not a String"
+      return
+    end
+
+    if @title.size > 100
+      @errors << "Title is too long"
+    end
+
+    if @title.size < 10
+      @errors << "Title is too short"
+    end
+
+    if @title.empty?
+      @errors << "Title is blank"
     end
   end
 
   def validate_cost
-    if !@cost.is_a?(Numeric)
+    unless @cost.is_a?(Numeric)
       @errors << "Cost is not a Numeric value"
+      return
+    end
+
+    if @cost.zero?
+      @errors << "Cost is zero"
+    end
+
+    if @cost < 0
+      @errors << "Cost is less than 0"
+    end
+
+    if @cost > 10000
+      @errors << "Cost is greater than 10.000"
     end
   end
 end
