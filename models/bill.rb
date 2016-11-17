30a0c6dc1b8660e7ca0a1a3ee85820a8a2782ec7
Ilya N. Zykin <zykin-ilya@ya.ru>
2016-11-17 23:19:12 +0300

    example 28

--- models/bill.rb
+++ models/bill.rb
@@ -12,14 +12,23 @@ class Bill
   def valid?
     @errors = []
 
+    validate_title
+    validate_cost
+
+    @errors.empty?
+  end
+
+  private
+
+  def validate_title
     if !@title.is_a?(String)
       @errors << "Title is not a String"
     end
+  end
 
+  def validate_cost
     if !@cost.is_a?(Numeric)
       @errors << "Cost is not a Numeric value"
     end
-
-    @errors.empty?
   end
 end
