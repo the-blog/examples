e007d10e54c7ffba20bfe3a01067c6dda9793b0d
Ilya N. Zykin <ilya.zykin@toptal.com>
2016-11-18 15:25:25 +0300

    example 32

---
+++ test/models/bill.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require_relative '../../models/bill'
+
+class TestBill < Minitest::Test
+  def setup
+    @bill = Bill.new
+  end
+
+  def test_title_after_create
+    assert_equal 'No Title', @bill.title
+  end
+
+  def test_cost_after_create
+    assert_equal 0.0, @bill.cost
+  end
+end
