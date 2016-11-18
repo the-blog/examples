3710ddc4507eb14cfbb8479bd54bfe432edfc1dd
Ilya N. Zykin <ilya.zykin@toptal.com>
2016-11-18 16:32:11 +0300

    example 39

--- helpers/app_helper.rb
+++ helpers/app_helper.rb
@@ -1,12 +1,24 @@
+def app_root
+  File.expand_path('../../', __FILE__)
+end
+
+def app_models_path
+  "#{ app_root }/models"
+end
+
+def app_processors_path
+  "#{ app_root }/processors"
+end
+
 def require_app_models
-  require_relative '../models/bill'
-  require_relative '../models/phone_bill'
-  require_relative '../models/transport_bill'
+  require "#{ app_models_path }/bill"
+  require "#{ app_models_path }/phone_bill"
+  require "#{ app_models_path }/transport_bill"
 end
 
 def require_app_processors
-  require_relative '../processors/exit_processor'
-  require_relative '../processors/main_command_processor'
-  require_relative '../processors/bills_processor'
-  require_relative '../processors/export_processor'
+  require "#{ app_processors_path }/exit_processor"
+  require "#{ app_processors_path }/main_command_processor"
+  require "#{ app_root }/bills_processor"
+  require "#{ app_processors_path }/export_processor"
 end
