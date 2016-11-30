46e7288c1115a6cb42b7dd7846bb6c93690a4736
Ilya N. Zykin <zykin-ilya@ya.ru>
2016-11-30 11:37:30 +0300

    example 59

--- test/models/storage_spec.rb
+++ test/models/storage_spec.rb
@@ -17,3 +17,26 @@ class TestStorage < Minitest::Test
     assert_equal @storage_1.object_id, @storage_2.object_id
   end
 end
+
+class TestStorageInterface < Minitest::Test
+  def test_add_valid_bill
+    bill = PhoneBill.new(cost: 10)
+    assert_equal storage.add_bill(bill), bill
+  end
+
+  def test_add_invalid_bill
+    bill = PhoneBill.new(cost: -1)
+
+    exception = assert_raises InvalidBill do
+      storage.add_bill(bill)
+    end
+
+    assert_match /Bill has validation errors/, exception.message
+  end
+
+  private
+
+  def storage
+    Storage.instance
+  end
+end
