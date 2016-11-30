e1aec4d47fd97971f69d7915ae27e0ec971eb964
Ilya N. Zykin <zykin-ilya@ya.ru>
2016-11-30 20:15:35 +0300

    example 60

--- test/models/bill_spec.rb
+++ test/models/bill_spec.rb
@@ -11,3 +11,15 @@ class TestBill < Minitest::Test
     assert_equal 'No Title', @bill.title
   end
 end
+
+class TestBillInitialize < Minitest::Test
+  def test_title_after_create
+    bill = Bill.new(title: 'Custom Title')
+    assert_equal 'Custom Title', bill.title
+  end
+
+  def test_cost_after_create
+    bill = Bill.new(cost: 33.3)
+    assert_equal 33.3, bill.cost
+  end
+end
